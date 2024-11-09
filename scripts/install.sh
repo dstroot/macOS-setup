fb6b40231ae525d5107f291f3ecc2bc449862eb2
Dan Stroot <Dan@Dans-iMac.home.lan>
2024-11-09 13:02:49 -0800

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #
 # This script should be run via curl:
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# or via wget:
-#   sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# or via fetch:
-#   sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#   sh -c "$(curl -fsSL sh -c "$(curl -fsSL https://raw.githubusercontent.com/dstroot/macos-setup/master/scripts/install.sh)")"
 #
+# or via wget:
+#   sh -c "$(wget -qO- sh -c "$(curl -fsSL https://raw.githubusercontent.com/dstroot/macos-setup/master/scripts/install.sh)")"
+#
+# or via fetch:
+#   sh -c "$(fetch -o - sh -c "$(curl -fsSL https://raw.githubusercontent.com/dstroot/macos-setup/master/scripts/install.sh)")"
 
 set -e
 
@@ -34,9 +35,23 @@ setup_color() {
   FMT_RESET=$(printf '\033[0m')
 }
 
+# logging functions
+fmt_info() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_BLUE}" "$*" "$FMT_RESET" >&2
+}
+
+fmt_warning() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_YELLOW}" "$*" "$FMT_RESET" >&2
+}
+
+fmt_success() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_GREEN}" "$*" "$FMT_RESET" >&2
+}
+
 fmt_error() {
   printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
 }
+
 
 setup_git() {
   command_exists git || {
@@ -47,31 +62,35 @@ setup_git() {
   cd $HOME
 }
 
-clone_dotfiles() { 
-    if [ ! -d "$HOME/.dotfiles" ]; then
-      echo "${FMT_BLUE}Cloning .dotfiles...${FMT_RESET}"
-      git clone https://github.com/dstroot/dotfiles.git .dotfiles
-      echo "${FMT_GREEN}....is now installed!${FMT_RESET}"
-    else
-      fmt_error ".dotfiles already exists!"
-    fi
+clone_repo() {
+  if [ ! -d "$HOME/$1" ]; then
+    fmt_info "Cloning $1..."
+    git clone https://github.com/dstroot/$1.git 
+    fmt_success "$1 cloned!"
+  else
+    fmt_error "$1 already exists!"
+  fi
 }
 
-clone_macos() { 
-    if [ ! -d "$HOME/.macos" ]; then
-      echo "${FMT_BLUE}Cloning .macos...${FMT_RESET}"
-      git clone https://github.com/dstroot/.macos.git .macos
-      echo "${FMT_GREEN}....is now installed!${FMT_RESET}"
-    else
-      fmt_error ".macos already exists!"
-    fi
+restore_mackup() {
+  if [ ! -f "$HOME"/.mackup.cfg ]; then
+    fmt_info "Restoring Mackup..."
+    cp "$HOME"/.dotfiles/mackup/.mackup.cfg ~/
+    yes Yes | mackup restore
+    fmt_success "Mackup restored!"
+  else
+    fmt_error "Mackup already restored!"
+  fi
 }
 
 main() {
   setup_color
   setup_git
-  clone_dotfiles
-  clone_macos
+
+  clone_repo ".dotfiles"
+  clone_repo ".macos"
+
+  restore_mackup
 }
 
 main "$@"
